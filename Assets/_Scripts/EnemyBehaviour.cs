c21c99878f18600a4fafdfdb57b009fceae6f675
Jonah Griffin <jonah.griffin@uoit.net>
2020-10-16 21:07:27 -0400

    Move kinda

---
+++ Assets/_Scripts/EnemyBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public GameObject player;
+    public float speed = 1;
+
+    Vector3 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = player.transform.position - this.transform.position;
+        move.Normalize();
+
+        this.transform.position += move * speed * Time.deltaTime;
+    }
+}
