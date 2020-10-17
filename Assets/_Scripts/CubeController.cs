ad789afc2f106ce76eb950cfdfc232e3c4e425d4
Steph <stephane.gagnon00@gmail.com>
2020-10-16 21:06:14 -0400

    event system start

---
+++ Assets/_Scripts/CubeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameEvents.current.onTouchCube += OnCubeColourChange;
+    }
+
+    private void OnCubeColourChange()
+    {
+        Color randColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        GetComponent<MeshRenderer>().material.SetColor("rand", randColor);
+    }
+}
