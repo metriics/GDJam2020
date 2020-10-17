78fd4b5e8b3aea873556e480eb6131c84c7b35c0
Steph <stephane.gagnon00@gmail.com>
2020-10-17 00:48:13 -0400

    finish all loot prefabs + loot ID tracking
    by default loot will be given ID of -1. ID should always be -1 unless loot is associated with a quest, in which case it should have an ID matching the ID of the quest. This helps if two people ask for help to find their missing object when both objects are of the same type.

---
+++ Assets/_Scripts/Loot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour
+{
+    private int lootID = -1;
+
+    public int GetLootID()
+    {
+        return lootID;
+    }
+
+    public void SetLootID(int id)
+    {
+        lootID = id;
+    }
+}
